0cebb552e064a791cc56a4ebf2bf17a51ee5e3b6
Hong Yi Wang <boomtski@hotmail.com>
2018-10-20 19:16:55 -0400

    GUI Fragments

---
+++ app/src/main/java/com/sensorem/overwatch/fragment/arm_alarm_Fragment.java
@@ -0,0 +1,36 @@
+package com.sensorem.overwatch.fragment;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.sensorem.overwatch.R;
+
+public class arm_alarm_Fragment extends DialogFragment {
+
+    private EditText armFragmentHourEditText;
+    private EditText armFragmentMinuteEditText;
+    private Button armFragmentSaveButton;
+    private Button armFragmentCancelButton;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflator, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View view = inflator.inflate(R.layout.activity_arm_alarm__fragment, container, false);
+
+        armFragmentHourEditText = view.findViewById(R.id.armFragmentHourEditText);
+        armFragmentMinuteEditText = view.findViewById(R.id.armFragmentMinuteEditText);
+        armFragmentSaveButton = view.findViewById(R.id.armFragmentSaveButton);
+        armFragmentCancelButton = view.findViewById(R.id.armFragmentCancelButton);
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/sensorem/overwatch/fragment/passcodeFragment.java
@@ -0,0 +1,35 @@
+package com.sensorem.overwatch.fragment;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.sensorem.overwatch.R;
+
+public class passcodeFragment extends DialogFragment {
+
+    private EditText passcodeEditText;
+    private Button passcodeConfirmButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflator, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View view = inflator.inflate(R.layout.activity_passcode_fragment, container, false);
+
+        passcodeEditText = view.findViewById(R.id.passcodeEditText);
+        passcodeConfirmButton = view.findViewById(R.id.passcodeConfirmButton);
+
+
+
+        return view;
+    }
+
+
+}
