64552fe1e8a098f60a0e09ed5119bf63acdd032d
jeffreytang97 <jeffreytang97@gmail.com>
2018-10-26 01:11:40 -0400

    login function, three dots menu changed, new java class created for codes

--- app/src/main/java/com/sensorem/overwatch/MainActivity.java
+++ app/src/main/java/com/sensorem/overwatch/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sensorem.overwatch;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected EditText codeEditText;
     protected Button loginButton;
 
+    public String theCode = "jeff131313";
+
+    codesSharedPreferences sharedPrefsLoginCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +26,47 @@ public class MainActivity extends AppCompatActivity {
         setupUI();
     }
 
+    protected void onStart()
+    {
+        super.onStart();
+
+        String aCode;
+        aCode = sharedPrefsLoginCode.getLoginCode();
+
+        // If the code exist in the sharedPreferences, it means the user didn't log out yet.
+        if(aCode != null)
+            goToAlarmActivity();
+
+        // When the user log out, the code data will be erased from the sharedPreferences
+    }
+
     protected void setupUI(){
+
+        // initialize shared preference object
+        sharedPrefsLoginCode = new codesSharedPreferences(MainActivity.this);
+
         codeEditText = findViewById(R.id.codeEditText);
         loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToAlarmActivity();
+            }
+        });
+    }
+
+    public void goToAlarmActivity(){
+
+        String code = codeEditText.getText().toString();
+
+        if (codeEditText.getText().toString().equals(theCode)){
+            sharedPrefsLoginCode.saveLoginCode(code); // save the code somewhere
+            Intent loginIntent = new Intent(MainActivity.this, alarmActivity.class);
+            startActivity(loginIntent);
+        }
+        else{
+            Toast.makeText(this, "Invalid code.", Toast.LENGTH_LONG).show();
+        }
     }
 }
