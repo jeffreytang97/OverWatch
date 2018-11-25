1ffdc6b6d5e3a5f06e8d3b3e33d0b2b5dca6b1ec
Hong Yi Wang <boomtski@hotmail.com>
2018-11-25 14:31:26 -0500

    Added fragment for confirmation

---
+++ app/src/main/java/com/sensorem/overwatch/fragment/confirmationFragment.java
@@ -0,0 +1,23 @@
+package com.sensorem.overwatch.fragment;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sensorem.overwatch.R;
+
+public class confirmationFragment extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
